9a15354bec51dd7071781d5e3e75151af7f4679c
EdmundoSR <desouzaribeiroedmundo@gmail.com>
2025-03-19 08:55:42 +0000

    Super_trunfo

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,58 @@
+# include <stdio.h>
 
+int main (){
+
+    char estado1, estado2;
+    char[5] código1, código2;
+    char[20] cidade1, cidade2,
+    int população1, população2;
+    float área1, área2;
+    float pib1, pib2;
+    int turisticos1, tusristicos2;
+
+    printf("Digite a letra do estado: /n");
+    scanf("%s",&estado1);
+
+    printf("Digite o código da carta: /n");
+    scanf("%s",&código1);
+
+    printf("Digite o nome da cidade: /n");
+    scanf("%s",&cidade1);
+
+    print("Digite a população: /n");
+    scanf("%d",&população1);
+
+    printf("Digite área em km²: /n");
+    scanf("%f",&área1);
+
+    printf("Digite o PIB: /n");
+    scanf("%f",&pib1);
+
+    printf("Digite números de pontos turísticos: /n");
+    scanf("%d",&turisticos1);
+
+    printf("Digite a letra do estado: /n");
+    scanf("%s",&estado2);
+
+    printf("Digite o código da carta: /n");
+    scanf("%s",&código2);
+
+    printf("Digite o nome da cidade: /n");
+    scanf("%s",&cidade2);
+
+    print("Digite a população: /n");
+    scanf("%d",&população2);
+
+    printf("Digite área em km²: /n");
+    scanf("%f",&área2);
+
+    printf("Digite o PIB: /n");
+    scanf("%f",&pib2);
+
+    printf("Digite números de pontos turísticos: /n");
+    scanf("%d",&turisticos2);
+
+    printf("")
+
+
+}
